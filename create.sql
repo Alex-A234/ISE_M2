12153c532090abf4b6e69413f1410aac0c731b8a
Yousif Faiq <Yousif.F@the-gw.com>
2025-04-08 23:28:31 +0200

    fix: correct typo and syntax

--- create.sql
+++ create.sql
@@ -1,78 +1,88 @@
 CREATE TABLE Seller (
-	SellerID INT PRIMARY KEY,
-	Name CHAR(200),
-	Description VARCHAR(16384)
-);
-
-CREATE TABLE Item (
-	ItemID INT PRIMARY KEY,
-	Name CHAR(200),
-	Price INT,
-	Rating INT,
-	Description VARCHAR(16384),
-	Available_Quantity INT,
-	FOREIGN KEY (SellerID) REFERENCES Seller(SellerID)
-);
-
-CREATE TABLE DvD (
-	ItemID INT PRIMARY KEY REFERENCES Item(ItemID), --IS-A Item
-	Duration INT,
-	Genre CHAR(64),
-	Age_Restiction INT
-);
-
-CREATE TABLE Book (
-	ItemID INT PRIMARY KEY REFERENCES Item(ItemID), --IS-A Item
-	Author CHAR(64),
-	ISBN CHAR(32),
-	Page_Count INT
-);
-
-CREATE TABLE Customer (
-	CustomerID INT PRIMARY KEY,
-	Name CHAR(64),
-	Phone_Number INT,
-	FOREIGN KEY (AddressID) REFERENCES Address(AddressID)
+    SellerID INT PRIMARY KEY,
+    Name CHAR(200),
+    Description TEXT(16384)
 );
 
 CREATE TABLE Address (
-	AddressID INT PRIMARY KEY,
-	Street CHAR(256),
-	City CHAR(128),
-	ZIP_Code INT
+    AddressID INT PRIMARY KEY,
+    Street VARCHAR(256),
+    City CHAR(128),
+    ZIP_Code INT
+);
+
+CREATE TABLE Item (
+    ItemID INT PRIMARY KEY,
+    Name CHAR(200),
+    Price INT,
+    Rating INT,
+    Description TEXT(16384),
+    Available_Quantity INT,
+    SellerID INT,
+    FOREIGN KEY (SellerID) REFERENCES Seller(SellerID)
+);
+
+CREATE TABLE DvD (
+    ItemID INT PRIMARY KEY REFERENCES Item(ItemID),
+    Duration INT,
+    Genre CHAR(64),
+    Age_Restriction INT
+);
+
+CREATE TABLE Book (
+    ItemID INT PRIMARY KEY REFERENCES Item(ItemID),
+    Author CHAR(64),
+    ISBN CHAR(32),
+    Page_Count INT
+);
+
+CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY,
+    Name CHAR(64),
+    Phone_Number INT,
+    AddressID INT,
+    FOREIGN KEY (AddressID) REFERENCES Address(AddressID)
 );
 
 CREATE TABLE Warehouse (
-	WarehouseID INT PRIMARY KEY,
-	Name CHAR(64),
-	Conact_Tel CHAR(20),
-	FOREIGN KEY (AddressID) REFERENCES Address(AddressID)
+    WarehouseID INT PRIMARY KEY,
+    Name CHAR(64),
+    Contact_Tel CHAR(20),
+    AddressID INT,
+    FOREIGN KEY (AddressID) REFERENCES Address(AddressID)
 );
 
 CREATE TABLE Shelf (
-	ShelfNr INT PRIMARY KEY,
-	_Section INT,
-	Capacity INT,
-	FOREIGN KEY (WarehouseID) REFERENCES Warehouse(WarehouseID)
+    WarehouseID INT,
+    ShelfNr INT,
+    _Section INT,
+    Capacity INT,
+    PRIMARY KEY (WarehouseID, ShelfNr),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouse(WarehouseID)
 );
 
 CREATE TABLE Item_Contains_Item (
-	FOREIGN Key ItemBundleID REFERENCES Item(ItemID),
-	FOREIGN Key ItemElementID REFERENCES Item(ItemID),
-	PRIMARY Key(ItemBundleID, ItemElementID)
+    ItemBundleID INT,
+    ItemElementID INT,
+    PRIMARY KEY (ItemBundleID, ItemElementID),
+    FOREIGN KEY (ItemBundleID) REFERENCES Item(ItemID),
+    FOREIGN KEY (ItemElementID) REFERENCES Item(ItemID)
 );
 
 CREATE TABLE Item_Stored_In_Shelf (
-	FOREIGN Key ItemID REFERENCES Item(ItemID),
-	FOREIGN Key ShelfNr REFERENCES Shelf(ShelfNr),
-	Quantity INT,
-	PRIMARY Key(ItemID, ShelfNr)
+    ItemID INT,
+    WarehouseID INT,
+    ShelfNr INT,
+    Quantity INT,
+    PRIMARY KEY (ItemID, WarehouseID, ShelfNr),
+    FOREIGN KEY (ItemID) REFERENCES Item(ItemID),
+    FOREIGN KEY (WarehouseID, ShelfNr) REFERENCES Shelf(WarehouseID, ShelfNr)
 );
 
 CREATE TABLE Customer_Buys_Item (
-	FOREIGN Key ItemID REFERENCES Item(ItemID),
-	FOREIGN Key CustomerID REFERENCES Customer(CustomerID),
-	PRIMARY Key(ItemID, CustomerID)
+    ItemID INT,
+    CustomerID INT,
+    PRIMARY KEY (ItemID, CustomerID),
+    FOREIGN KEY (ItemID) REFERENCES Item(ItemID),
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
-
-INSERT INTO Seller (1, "Steve", "sells stuff");
