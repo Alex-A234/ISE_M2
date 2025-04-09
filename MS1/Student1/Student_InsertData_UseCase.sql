2204d31d93a723c29afd175abd3bac1aea4dfdfd
Yousif Faiq <Yousif.F@the-gw.com>
2025-04-09 13:23:16 +0200

    refactor: improve comments for clarity in Student_InsertData_UseCase.sql

--- MS1/Student1/Student_InsertData_UseCase.sql
+++ MS1/Student1/Student_InsertData_UseCase.sql
@@ -1,20 +1,23 @@
+-- This script inserts data into the database for a use case where a customer buys items from different warehouses.
 
--- Customer 1001 buys Sissi DVD Box (ItemID 101)
+SELECT * FROM Item;
+SELECT * FROM Item_Stored_In_Shelf;
+
+-- Insert data into Customer_Buys_Item table
 INSERT INTO Customer_Buys_Item (ItemID, CustomerID, Quantity, PurchasePrice)
 SELECT 101, 1001, 1, Price FROM Item WHERE ItemID = 101;
 
--- Customer 1001 buys Österreich Kochbuch (ItemID 102)
 INSERT INTO Customer_Buys_Item (ItemID, CustomerID, Quantity, PurchasePrice)
 SELECT 102, 1001, 1, Price FROM Item WHERE ItemID = 102;
 
--- Update inventory quantities (warehouse shelves)
+-- Update the quantity in the Item_Stored_In_Shelf table
 UPDATE Item_Stored_In_Shelf SET Quantity = Quantity - 1 
 WHERE ItemID = 101 AND WarehouseID = 1 AND ShelfNr = 1;
 
 UPDATE Item_Stored_In_Shelf SET Quantity = Quantity - 1 
 WHERE ItemID = 102 AND WarehouseID = 2 AND ShelfNr = 1;
 
--- Update global item quantities
+-- Update the available quantity in the Item table
 UPDATE Item SET Available_Quantity = Available_Quantity - 1 
 WHERE ItemID = 101 AND Available_Quantity >= 1;
 
