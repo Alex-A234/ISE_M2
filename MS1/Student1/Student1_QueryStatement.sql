2d0d112401ceee4a997726ec9fbccb52354b03c8
Yousif Faiq <Yousif.F@the-gw.com>
2025-04-09 13:26:18 +0200

    refactor: replace old queries with a consolidated query to find total items purchased by each customer

--- MS1/Student1/Student1_QueryStatement.sql
+++ MS1/Student1/Student1_QueryStatement.sql
@@ -1,24 +1,17 @@
--- Check purchases with new columns
-SELECT 
-    c.Name AS Customer, 
-    i.Name AS Item, 
-    i.Description,
-    cbi.Quantity,
-    cbi.PurchasePrice,
-    cbi.PurchaseDate
-FROM Customer_Buys_Item cbi
-JOIN Customer c ON cbi.CustomerID = c.CustomerID
-JOIN Item i ON cbi.ItemID = i.ItemID
-WHERE cbi.CustomerID = 1001;
-
--- Verify inventory with purchase context
-SELECT 
-    i.ItemID, 
-    i.Name, 
-    i.Available_Quantity AS Total_Available,
-    iss.Quantity AS Shelf_Quantity,
-    iss.WarehouseID,
-    iss.ShelfNr
-FROM Item i
-JOIN Item_Stored_In_Shelf iss ON i.ItemID = iss.ItemID
-WHERE i.ItemID IN (101, 102);
+-- Query Statement to find the total number of items purchased by each customer
+SELECT  
+    c.CustomerID,  
+    c.Name AS Customer,  
+    i.Name AS Item,  
+    i.Price,  
+    s.Name AS Seller,  
+    w.Name AS Warehouse,  
+    i.Available_Quantity AS RemainingStock  
+FROM Customer_Buys_Item cbi  
+JOIN Customer c ON cbi.CustomerID = c.CustomerID    
+JOIN Item i ON cbi.ItemID = i.ItemID                
+JOIN Seller s ON i.SellerID = s.SellerID            
+JOIN Item_Stored_In_Shelf iss ON i.ItemID = iss.ItemID  
+JOIN Warehouse w ON iss.WarehouseID = w.WarehouseID    
+WHERE c.CustomerID = 1001    
+ORDER BY i.Price DESC;  
