6b845a98cc9fdbbbdc8e6ba344b58a35d1ff0c17
Yousif Faiq <Yousif.F@the-gw.com>
2025-04-09 04:17:11 +0200

    refactor: update SQL query comments for clarity and include new columns in purchase selection

--- MS1/Student1/Student1_QueryStatement.sql
+++ MS1/Student1/Student1_QueryStatement.sql
@@ -1,11 +1,17 @@
--- Check the purchases were recorded
-SELECT c.Name AS Customer, i.Name AS Item, i.Description
+-- Check purchases with new columns
+SELECT 
+    c.Name AS Customer, 
+    i.Name AS Item, 
+    i.Description,
+    cbi.Quantity,
+    cbi.PurchasePrice,
+    cbi.PurchaseDate
 FROM Customer_Buys_Item cbi
 JOIN Customer c ON cbi.CustomerID = c.CustomerID
 JOIN Item i ON cbi.ItemID = i.ItemID
 WHERE cbi.CustomerID = 1001;
 
--- Verify inventory updates
+-- Verify inventory with purchase context
 SELECT 
     i.ItemID, 
     i.Name, 
