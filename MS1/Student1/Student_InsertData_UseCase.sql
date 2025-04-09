0325a10374ca5f532213765be2932585710883d4
Yousif Faiq <Yousif.F@the-gw.com>
2025-04-09 04:10:38 +0200

    feat: enhance Customer_Buys_Item insertions with Quantity and PurchasePrice, improve comments for clarity

--- MS1/Student1/Student_InsertData_UseCase.sql
+++ MS1/Student1/Student_InsertData_UseCase.sql
@@ -1,17 +1,22 @@
 
--- Customer 1001 (Huber Anna) buys the Sissi DVD Box (ItemID 101)
-INSERT INTO Customer_Buys_Item (ItemID, CustomerID) VALUES (101, 1001);
+-- Customer 1001 buys Sissi DVD Box (ItemID 101)
+INSERT INTO Customer_Buys_Item (ItemID, CustomerID, Quantity, PurchasePrice)
+SELECT 101, 1001, 1, Price FROM Item WHERE ItemID = 101;
 
--- Customer 1001 also buys the Österreich Kochbuch (ItemID 102)
-INSERT INTO Customer_Buys_Item (ItemID, CustomerID) VALUES (102, 1001);
+-- Customer 1001 buys Österreich Kochbuch (ItemID 102)
+INSERT INTO Customer_Buys_Item (ItemID, CustomerID, Quantity, PurchasePrice)
+SELECT 102, 1001, 1, Price FROM Item WHERE ItemID = 102;
 
--- Update inventory quantities
+-- Update inventory quantities (warehouse shelves)
 UPDATE Item_Stored_In_Shelf SET Quantity = Quantity - 1 
 WHERE ItemID = 101 AND WarehouseID = 1 AND ShelfNr = 1;
 
 UPDATE Item_Stored_In_Shelf SET Quantity = Quantity - 1 
 WHERE ItemID = 102 AND WarehouseID = 2 AND ShelfNr = 1;
 
--- Update the main item quantities
-UPDATE Item SET Available_Quantity = Available_Quantity - 1 WHERE ItemID = 101;
-UPDATE Item SET Available_Quantity = Available_Quantity - 1 WHERE ItemID = 102;
+-- Update global item quantities
+UPDATE Item SET Available_Quantity = Available_Quantity - 1 
+WHERE ItemID = 101 AND Available_Quantity >= 1;
+
+UPDATE Item SET Available_Quantity = Available_Quantity - 1 
+WHERE ItemID = 102 AND Available_Quantity >= 1;
