b6e065d1e494ce631657effc5c38392fb790c562
Alex-A234 <alex.amstetter@gmail.com>
2025-04-09 16:58:07 +0200

    corrected typos in report

--- MS1/Student2/Student2_InsertData_UseCase.sql
+++ MS1/Student2/Student2_InsertData_UseCase.sql
@@ -2,10 +2,10 @@
 -- adding an existing item to a shelf where it has never before been stored at.
 INSERT INTO Item_Stored_In_Shelf(ItemID, WarehouseID, ShelfNr, Quantity) VALUES (4, 2, 2, 22);
 -- also have to update the available quantity of the item for consistency.
-UPDATE Item SET Available_Quantity = Available_Quantity+22 WHERE ItemID = 4 AND WarehouseID = 2 AND ShelfNr = 2;
+UPDATE Item SET Available_Quantity = Available_Quantity+22 WHERE ItemID = 4;
 
 -- case 2:
 -- adding an existing item to a shelf where it has been stored at before, meaning we can't create a new entry but have to update an existing entry.
 UPDATE Item_Stored_In_Shelf SET Quantity = Quantity+22 WHERE ItemID = 4 AND WarehouseID = 2 AND ShelfNr = 2;
 -- also have to update the available quantity of the item for consistency.
-UPDATE Item SET Available_Quantity = Available_Quantity+22 WHERE ItemID = 4 AND WarehouseID = 2 AND ShelfNr = 2;
+UPDATE Item SET Available_Quantity = Available_Quantity+22 WHERE ItemID = 4;
